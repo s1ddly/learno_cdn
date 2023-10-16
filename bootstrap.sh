ac5fb971c34961ff30c5aa4b47256bfe3afb83e0
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2023-10-16 20:13:46 +1100

    adding bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+# Bootstrap script for the LearnAi website
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+# Installing/enabling ssh
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+sudo apt update
+sudo apt install openssh-server
+sudo systemctl enable ssh
+sudo ufw allow ssh
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+# Installing other linux dependencies
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+sudo apt install php8.1
+sudo apt install apache2
+sudo apt install mysql-server
+sudo apt install php-cli
+sudo apt install php-xml
+sudo apt install php-common
+sudo apt install php-curl
+sudo apt install php8.1-cli
+sudo apt install php8.1-common
+sudo apt install php8.1-curl
+sudo apt install php8.1-mysql
+sudo apt install php8.1-opcache
+sudo apt install php8.1-readline
+sudo apt install php8.1-xml
+sudo apt install curl
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+# Installing Composer
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+sudo service apache2 restart
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+# Setting Up mysql
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+sudo systemctl stop mysql
+sudo cat << EOF  > /etc/mysql/temp.cnf
+[mysqld]
+skip-grant-tables
+skip-networking
+EOF
+
+cat << EOF > /tmp/init.sql
+CREATE USER 'main'@'localhost';
+CREATE DATABASE main;
+GRANT ALL PRIVILEGES ON main.* TO 'main'@'localhost';
+EOF
+
+sudo mysql -u root < /tmp/init.sql
