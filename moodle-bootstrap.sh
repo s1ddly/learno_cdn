01422b69aa079447ac73676d75dc81eebf42cbc6
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2024-02-25 16:19:22 +1100

    testing moodle bootstrap

--- moodle-bootstrap.sh
+++ moodle-bootstrap.sh
@@ -11,10 +11,18 @@
 sudo apt -y install php8.1
 sudo apt -y install apache2
 sudo apt -y install mysql-server
+sudo apt -y install mysql-client
 sudo apt -y install php-cli
-sudo apt -y install php8.1-mysql
-sudo apt -y install php8.1-xml
+sudo apt -y install php-mysql
+sudo apt -y install php-mbstring
+sudo apt -y install php-xml
+sudo apt -y install php-curl
+sudo apt -y install php-zip
+sudo apt -y install php-gd
+sudo apt -y install php-intl
+sudo apt -y install php-soap
 sudo apt -y install curl
+sudo apt -y install vim
 sudo apt -y install vim
 
 #-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
@@ -64,7 +72,9 @@ sudo get https://download.moodle.org/download.php/direct/stable403/moodle-latest
 sudo unzip -q moodle-latest-403.zip
 sudo rm moodle-latest-403.zip
 sudo chown $USER:$USER moodle
-/usr/bin/php /var/www/html/moodle/admin/cli/install.php
+sudo mkdir /var/www/moodledata
+sudo chown www-data:www-data /var/www/moodledata
+#/usr/bin/php /var/www/html/moodle/admin/cli/install.php
 
 #-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 # Configure Moodle
@@ -81,7 +91,7 @@ $CFG->dblibrary = 'native';
 $CFG->dbhost    = 'localhost';
 $CFG->dbname    = 'main';
 $CFG->dbuser    = 'main';
-$CFG->dbpass    = '$PWORD';
+$CFG->dbpass    = 'yMtEtYDnL8VkDhu8';
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
@@ -101,3 +111,26 @@ require_once(__DIR__ . '/lib/setup.php');
 // There is no php closing tag in this file,
 // it is intentional because it prevents trailing whitespace problems!
 EOF
+
+--fullname=STRING     The fullname of the site
+--shortname=STRING    The shortname of the site
+--summary=STRING      The summary to be displayed on the front page
+--adminuser=USERNAME  Username for the moodle admin account. Default is admin
+--adminpass=PASSWORD  Password for the moodle admin account,
+                      required in non-interactive mode.
+--adminemail=STRING   Email address for the moodle admin account.
+--sitepreset=STRING   Admin site preset to be applied during the installation process.
+--supportemail=STRING Email address for support and help.
+--upgradekey=STRING   The upgrade key to be set in the config.php, leave empty to not set it.
+--non-interactive     No interactive questions, installation fails if any
+                      problem encountered.
+--agree-license       Indicates agreement with software license,
+                      required in non-interactive mode.
+--allow-unstable      Install even if the version is not marked as stable yet,
+                      required in non-interactive mode.
+--skip-database       Stop the installation before installing the database.
+-h, --help            Print out this help
+
+
+
+php /var/www/html/moodle/admin/cli/install.php --chmod=2777 --lang=en --wwwroot=http://localhost:80 --dataroot=/data/moodle --dbtype=mysqli --dbhost=localhost --dbname=main --dbuser=main --dbpass=yMtEtYDnL8VkDhu8 --prefix=mdl_ --dbport=3306 --fullname=learno --shortname=learno --adminuser=admin --adminpass=password --non-interactive --agree-license
