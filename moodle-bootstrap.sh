ac7ba43ffce155d4e3cc9ebef1729170a4a23771
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2024-03-05 23:10:58 +1100

    using custom code instead of moodle published code

--- moodle-bootstrap.sh
+++ moodle-bootstrap.sh
@@ -63,7 +63,7 @@ sleep 10
 #-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 # Setting PHP Vars
 #-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
-sudo sed -i '426s/.*/max_input_vars = 200000/' /etc/php/8.1/apache2/php.ini
+sudo sed -i '426s/.*/max_input_vars = 5000/' /etc/php/8.1/apache2/php.ini
 sudo systemctl restart apache2
 
 #-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
@@ -71,17 +71,19 @@ sudo systemctl restart apache2
 #-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 cd /var/www/html/
 sudo rm index.html
-sudo wget https://download.moodle.org/download.php/direct/stable403/moodle-latest-403.zip
-sudo unzip -q moodle-latest-403.zip
-sudo rm moodle-latest-403.zip
+sudo wget https://github.com/s1ddly/moodle/archive/refs/heads/main.zip
+sudo unzip -q main.zip
+sudo mv moodle-main/ moodle
+sudo rm main.zip
 sudo chown -R $USER:$USER moodle
 sudo mkdir /var/www/moodledata
 sudo chown www-data:www-data /var/www/moodledata
+sudo chmod 777 /var/www/moodledata
 
 #-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 # Configure Moodle
 #-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 cd /var/www/html/moodle
-sed -i '2783s/.*/        $result->setStatus(true)/' /var/www/html/moodle/lib/upgradelib.php
+#sed -i '2783s/.*/        $result->setStatus(true)/' /var/www/html/moodle/lib/upgradelib.php
 php /var/www/html/moodle/admin/cli/install.php --chmod=2777 --lang=en --wwwroot=http://localhost:80 --dataroot=/var/www/moodledata --dbtype=mysqli --dbhost=localhost --dbname=main --dbuser=main --dbpass=$PWORD --prefix=mdl_ --dbport=3306 --fullname=learno --shortname=learno --adminuser=admin --adminpass=password --non-interactive --agree-license
 #php /var/www/html/moodle/admin/cli/install_database.php --lang=en --fullname=learno --shortname=learno --summary=learno --supportemail=test@gmail.com --adminuser=admin --adminpass=Password_1 --adminemail=admin@testmail.com --agree-license
